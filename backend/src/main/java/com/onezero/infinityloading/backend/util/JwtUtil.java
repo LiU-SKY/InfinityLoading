d930ad786c94eeb4701108249107b80bec08479e
gitam <gitamsky1@naver.com>
2025-07-08 23:41:21 +0900

    비밀키 인코딩, 문자열 수정

--- backend/src/main/java/com/onezero/infinityloading/backend/util/JwtUtil.java
+++ backend/src/main/java/com/onezero/infinityloading/backend/util/JwtUtil.java
@@ -1,21 +1,27 @@
 package com.onezero.infinityloading.backend.util;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private static final String SECRET = "mySecretKeyisveryveryveryveryveryveryverylong";
+    private static final String SECRET = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF";
     private static final long EXPIRATION_TIME = 86400000; // 1일
+
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
 
     public String generateToken(String username) { /*JWT 토큰을 생성하여 반환*/
         return Jwts.builder()
                 .setSubject(username)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
     }
 
