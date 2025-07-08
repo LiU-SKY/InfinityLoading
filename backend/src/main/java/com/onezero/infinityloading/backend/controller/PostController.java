2da2556f56d5e85939cc70c7b33c1fd7abdc64f8
gitam <gitamsky1@naver.com>
2025-07-08 21:31:20 +0900

    CRUD 기능 기초 구현

--- backend/src/main/java/com/onezero/infinityloading/backend/controller/PostController.java
+++ backend/src/main/java/com/onezero/infinityloading/backend/controller/PostController.java
@@ -1,6 +1,32 @@
 package com.onezero.infinityloading.backend.controller;
 
+import com.onezero.infinityloading.backend.entity.Post;
+import com.onezero.infinityloading.backend.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
 
 public class PostController {
+    private final PostService postService;
+
+    @PostMapping
+    public Post create(@RequestBody Post post) { return postService.save(post); }
+
+    @GetMapping
+    public List<Post> readAll() { return postService.findAll(); }
+
+    @GetMapping("{id}")
+    public Post readOne(@PathVariable Long id) { return postService.findById(id); }
+
+    @PutMapping("/{id}")
+    public Post update(@PathVariable Long id, @RequestBody Post post) { return postService.update(id, post); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { postService.delete(id); }
+
 }
