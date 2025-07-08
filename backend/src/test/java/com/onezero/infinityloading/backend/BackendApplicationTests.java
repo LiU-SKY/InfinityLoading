02b1cc378e45b4bf6278b3404eec2d8eb7de571a
gitam <gitamsky1@naver.com>
2025-07-08 19:49:25 +0900

    리엑트, 스프링부트 템플릿 생성

---
+++ backend/src/test/java/com/onezero/infinityloading/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onezero.infinityloading.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
