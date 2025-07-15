11af25192c995be4fb9fc9b1e070238c52e59ab5
gitam <gitamsky1@naver.com>
2025-07-16 01:30:19 +0900

    [fix] 조회수 버그 수정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,5 @@ import App from './App.jsx'; // App 컴포넌트를 불러옴
 import './index.css'; // (있다면 CSS도 불러오기)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
